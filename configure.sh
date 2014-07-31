65b23d689c38ee0e108626c608f4ecf696ead0e0
Vjekoslav Giacometti <vjeko@entropia.hr>
2014-07-31 22:18:34 +0200

    Added saucelabs testing functionality

--- configure.sh
+++ configure.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 #configuring the system
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/start_test.js
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/Makefile
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/start_test.js
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/phantom_runner.js
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/saucelabs_runner.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/package.json
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
 
 #installing meteorite
 npm install -g meteorite
+
+#installing dependencies
+npm install
 
 mrt update
