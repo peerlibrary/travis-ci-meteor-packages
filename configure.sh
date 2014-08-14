b1fc7474ae40ee960133c3cd433def9d3a279f7e
Vjekoslav Giacometti <vjeko@entropia.hr>
2014-08-14 17:59:43 +0200

    Updated config script to match new files and paths

--- configure.sh
+++ configure.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-#configuring the system
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/Makefile
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/start_test.js
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/phantom_runner.js
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/saucelabs_runner.coffee
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/package.json
+#creating directory structure
+mkdir .test_scripts
 
-#install meteor
+#downloading scripts
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/Makefile
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/package.json
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/start_test.coffee
+
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_runner.js -O .test_scripts/phantom_wrapper.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_runner.js -O .test_scripts/phantom_runner.js
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/saucelabs_runner.coffee -O .test_scripts/saucelabs_wrapper.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_runner.js -O .test_scripts/saucelabs_runner.coffee
+
+#installing meteor
 curl https://install.meteor.com | /bin/sh
 
 #installing meteorite
 npm install -g meteorite
 
-#installing dependencies
+#installing dependencies defined in package.json
 npm install
 
 mrt update
