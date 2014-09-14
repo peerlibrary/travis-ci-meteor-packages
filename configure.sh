6216d794832e5a8e7de4fb3689348c51f7f214f2
Vjekoslav Giacometti <vjeko@entropia.hr>
2014-09-14 18:13:44 +0200

    Updated config script to download new JS files.

--- configure.sh
+++ configure.sh
@@ -6,12 +6,12 @@ mkdir .test_scripts
 #downloading scripts
 wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/Makefile
 wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/package.json
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/start_test.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/start_test.js
 
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_wrapper.coffee -O .test_scripts/phantom_wrapper.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_wrapper.js -O .test_scripts/phantom_wrapper.js
 wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/phantom_runner.js -O .test_scripts/phantom_runner.js
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/saucelabs_wrapper.coffee -O .test_scripts/saucelabs_wrapper.coffee
-wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/saucelabs_runner.coffee -O .test_scripts/saucelabs_runner.coffee
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/saucelabs_wrapper.js -O .test_scripts/saucelabs_wrapper.js
+wget --no-cache https://raw.github.com/peerlibrary/travis-ci-meteor-packages/saucelabs/test_scripts/saucelabs_runner.js -O .test_scripts/saucelabs_runner.js
 
 #installing meteor
 curl https://install.meteor.com | /bin/sh
